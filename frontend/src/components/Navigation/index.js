8a7eef8718a6afb9349680be893d8b0ea7abd7b5
Josh Pascual <joshapascual@gmail.com>
2024-02-12 16:46:16 -0800

    mobile made

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -12,7 +12,7 @@ import { Tooltip, TooltipRefProps } from 'react-tooltip';
 export default function Navigation() {
     const navigate = useNavigate();
 
-    // const [openMobileNav, setOpenMobileNav] = useState(false);
+    const [openMobileNav, setOpenMobileNav] = useState(false);
     // const [openAbout, setOpenAbout] = useState(false);
     // const [showTooltip, setShowTooltip] = useState(true);
     const { showAccessibility, setShowAccessibility } = useAccessibilityModal();
@@ -32,34 +32,91 @@ export default function Navigation() {
     // }, [])
 
     return (
-        <div className='px-20'>
-            {/* Desktop */}
-            <div className='flex justify-between items-end py-6'>
+        <div className='px-4 md:px-20 border-b md:border-none border-gray-300 pb-2'>
+            <div className='flex justify-between items-end py-4 md:py-6'>
                 <button onClick={() => {}} className='flex gap-3 items-end'>
                     <img src={logo} alt='' className='h-12 w-12' />
-                    <h1 className='text-4xl font-semibold text-cyan-600 tracking-tight text-end'><span className='text-5xl font-extrabold'>Friends</span> of Columbus Square</h1>
+                    <div className='hidden md:flex items-end'>
+                        <h1 className='gap-1 text-4xl font-semibold text-cyan-600 tracking-tight text-end'><span className='text-5xl font-extrabold'>Friends</span> of Columbus Square</h1>
+                    </div>
+                    <div className='md:hidden flex flex-col justify-end items-start'>
+                        <h1 className='gap-1 text-lg font-semibold text-cyan-600 tracking-tight text-end leading-3'><span className='text-2xl font-extrabold'>Friends</span> of</h1>
+                        <h1 className='gap-1 text-lg font-semibold text-cyan-600 tracking-tight text-end leading-4'>Columbus Square</h1>
+                    </div>
                 </button>
+
+                {/* Desktop */}
                 <div className='hidden md:grid grid-cols-5 w-2/5 items-end'>
-                    <button onClick={() => {}} className='font-semibold text-xl  hover:text-rose-500'>
+                    <button onClick={() => navigate("/about")} className={`font-semibold text-xl hover:text-rose-500 ${page === "about" && "text-cyan-700"}`}>
                         About
                     </button>
-                    <button onClick={() => {}} className='font-semibold text-xl  hover:text-rose-500'>
+                    <button onClick={() => navigate("/events")} className={`font-semibold text-xl hover:text-rose-500 ${page === "events" && "text-cyan-700"}`}>
                         Events
                     </button>
-                    <button onClick={() => {}} className='font-semibold text-xl  hover:text-rose-500'>
+                    <button onClick={() => navigate("/donate")} className={`font-semibold text-xl hover:text-rose-500 ${page === "donate" && "text-cyan-700"}`}>
                         Donate
                     </button>
-                    <button onClick={() => {}} className='font-semibold text-xl  hover:text-rose-500'>
+                    <button onClick={() => navigate("/gallery")} className={`font-semibold text-xl hover:text-rose-500 ${page === "gallery" && "text-cyan-700"}`}>
                         Gallery
                     </button>
-                    <button onClick={() => {}} className='font-semibold text-xl  hover:text-rose-500'>
+                    <button onClick={() => navigate("/contact")} className={`font-semibold text-xl hover:text-rose-500 ${page === "contact" && "text-cyan-700"}`}>
                         Contact
                     </button>
                 </div>
+
+                {/* Mobile */}
+                <div className="md:hidden z-50" id="nav-tooltip-mobile">
+                    <Menu as="div" className="relative inline-block text-left">
+                        <div>
+                            <Menu.Button onClick={() => setOpenMobileNav(!openMobileNav)} className="rounded-lg focus:bg-slate-200 p-3">
+                                <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                                </svg>
+                            </Menu.Button>
+                        </div>
+                        <Transition
+                            as={Fragment}
+                            // enter="transition ease-out duration-100"
+                            // enterFrom="transform opacity-0 scale-95"
+                            // enterTo="transform opacity-100 scale-100"
+                            // leave="transition ease-in duration-75"
+                            // leaveFrom="transform opacity-100 scale-100"
+                            // leaveTo="transform opacity-0 scale-95"
+                            enter="transition ease-out duration-300 transform"
+                            enterFrom="opacity-0 -translate-y-full scale-95"
+                            enterTo="opacity-100 translate-y-0 scale-100"
+                            leave="transition ease-in duration-200 transform"
+                            leaveFrom="opacity-100 translate-y-0 scale-100"
+                            leaveTo="opacity-0 -translate-y-full scale-95"
+                        >
+                            <Menu.Items className="fixed top-28 right-0 w-screen origin-top divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5">
+                                <div className='flex flex-col'>
+                                    <Menu.Item>
+                                        <Link to="/about" className="py-3 text-center focus:bg-slate-200 border-b border-slate-200">About</Link>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        <Link to="/events" className="py-3 text-center focus:bg-slate-200 border-b border-slate-200">Events</Link>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        <Link to="/donate" className="py-3 text-center focus:bg-slate-200 border-b border-slate-200">Donate</Link>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        <Link to="/gallery" className="py-3 text-center focus:bg-slate-200 border-b border-slate-200">Gallery</Link>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        <Link to="/contact" className="py-3 text-center focus:bg-slate-200 border-b border-slate-200">Contact</Link>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        <button onClick={() => setShowAccessibility(true)} className="py-3 text-center focus:bg-slate-200">
+                                            Accessibility
+                                        </button>
+                                    </Menu.Item>
+                                </div>
+                            </Menu.Items>
+                        </Transition>
+                    </Menu>
+                </div>
             </div>
-
-            {/* Mobile */}
-
         </div>
     )
 }
