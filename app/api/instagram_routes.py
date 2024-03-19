72fd5e31881d2eb4b7e3514fcd23b4ef806eeb30
Josh Goldenberg <129535504+jgoldenberg29@users.noreply.github.com>
2024-03-19 14:04:20 -0400

    basic instagram fetch route from flask, gallery store file with thunk

---
+++ app/api/instagram_routes.py
@@ -0,0 +1,53 @@
+from flask import Blueprint, session, request
+import os
+from app.models import db
+from flask_login import login_required, current_user
+from icecream import ic
+
+instagram_routes = Blueprint('instagram', __name__)
+
+@instagram_routes.route('/auth')
+def authenticate():
+    '''
+    Autherization window for instagram user
+    https://www.columbussquare.org/instagram/auth
+    https://api.instagram.com/oauth/authorize
+    ?client_id=**************
+    &redirect_uri=https://www.columbussquare.org/auth
+    &scope=user_profile,user_media
+    &response_type=code
+
+    code=*****************************
+
+    ***post request for short term access token***
+    curl -X POST \
+    https://api.instagram.com/oauth/access_token \
+    -F client_id=************** \
+    -F client_secret=************* \
+    -F grant_type=authorization_code \
+    -F redirect_uri=https://www.columbussquare.org/auth \
+    -F code=*******************
+
+    ***response***
+    "access_token": "**********************",
+    "user_id": 7495916713801776
+    expires at: ~ 2:20pm
+
+    ***request for userId with short term access token***
+    curl -X GET \
+    'https://graph.instagram.com/************?fields=id,username&access_token=**********************'
+
+    ***response***
+    {"id":"**********************","username":"goldylocks1992"}
+
+    ***request for long-lived token***
+    curl -X GET \
+    'https://graph.instagram.com/access_token?grant_type=ig_exchange_token&&client_secret=*****************&access_token=**********************'
+
+    ***response***
+    {"access_token":********************","token_type":"bearer","expires_in":5183999}
+
+    '''
+
+    data = get.request(f'https://graph.instagram.com/me/media?fields=id,caption,media_url&access_token={os.environ.get(IG_ACCESS_KEY)}')
+    ic(data)
--- frontend/src/store/gallery.js
+++ frontend/src/store/gallery.js
@@ -1,32 +1,25 @@
-// import { createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import {setUser} from "./session"
 
-// const initialState = {
-//     imageIds: []
-// }
+const ALL_IMAGES = "images/getAll"
 
-// const gallerySlice = createSlice({
-//     name: 'gallery',
-//     initialState,
-//     reducers: {
-//     },
-//     extraReducers: (builder) => {
-//         builder.addCase('allData/getAllData', (state, action) => {
-//             state.imageIds = action.payload.imageIds
-//         });
 
-//         builder.addCase('images/add', (state, action) => {
-//             if (action.payload.gallery === true){
-//                 state.imageIds.push(action.payload.id)
-//             }
-//         });
+export const getAllImages = (images) => {
+    return {
+        type: ALL_IMAGES,
+        images
+    }
+}
 
-//         builder.addCase('images/remove', (state, action) => {
-//             const index = state.imageIds.indexOf(action.payload)
-//             if (index !== -1) {
-//                 state.imageIds.splice(index, 1)
-//             }
-//         })
-//     }
-// })
 
-// export default gallerySlice.reducer
+export const thunkGetAllImages = () => async dispatch => {
+    const res = await fetch('api/instagram/auth')
+
+    if (res.ok) {
+        const data = await res.json()
+        dispatch(getAllImages(data.images))
+        return null
+    } else {
+        const data = await res.json()
+        return data
+    }
+}
