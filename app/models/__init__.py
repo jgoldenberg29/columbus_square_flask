3a4b5d35388b99ae4cf9c3c078408452bfd21b66
Josh Pascual <joshapascual@gmail.com>
2024-04-02 13:58:12 -0700

    deleted doc files

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,5 @@
 from .db import db, environment, SCHEMA
 from .user import User
 from .events import Event
-# from .documents import Document
-# from .document_items import DocumentItem
 from .news import News
 from .images import Image
