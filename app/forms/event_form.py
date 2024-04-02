2145c06b3769e48ef045862e5aedd0cd0ef21a2e
Josh Pascual <joshapascual@gmail.com>
2024-04-02 13:10:20 -0700

    updated event form import

--- app/forms/event_form.py
+++ app/forms/event_form.py
@@ -3,7 +3,7 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, DateTimeField, TimeField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Email, ValidationError, Length, URL
 from app.models import Event
-from ..api.aws_helpers import ALLOWED_EXTENSIONS
+# from app.api.aws_helpers import ALLOWED_EXTENSIONS
 
 locations = [
     "Whole Park",
