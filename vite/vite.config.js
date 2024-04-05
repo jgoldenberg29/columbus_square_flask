7a029e27601bd29bab6b2dcd948f14ff33878781
Josh Pascual <joshapascual@gmail.com>
2024-04-05 12:42:10 -0700

    updated vite config

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import fs from 'fs/promises';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,4 +27,33 @@ export default defineConfig({
       },
     },
   },
+  resolve: {
+    alias: {
+      src: resolve(__dirname, 'src'),
+    },
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: 'load-js-files-as-jsx',
+          setup(build) {
+            build.onLoad(
+              { filter: /src\\.*\.js$/ },
+              async (args) => ({
+                loader: 'jsx',
+                contents: await fs.readFile(args.path, 'utf8'),
+              })
+            );
+          },
+        },
+      ],
+    },
+  },
+
 })
