4b67b48a33f4f4ee99d644de69286a7a0adae852
Josh Pascual <joshapascual@gmail.com>
2024-04-08 12:31:55 -0700

    added optimizeDeps config

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -41,23 +41,23 @@ export default defineConfig({
     include: /src\/.*\.jsx?$/,
     exclude: [],
   },
-  // optimizeDeps: {
-  //   esbuildOptions: {
-  //     plugins: [
-  //       {
-  //         name: 'load-js-files-as-jsx',
-  //         setup(build) {
-  //           build.onLoad(
-  //             { filter: /src\\.*\.js$/ },
-  //             async (args) => ({
-  //               loader: 'jsx',
-  //               contents: await fs.readFile(args.path, 'utf8'),
-  //             })
-  //           );
-  //         },
-  //       },
-  //     ],
-  //   },
-  // },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: 'load-js-files-as-jsx',
+          setup(build) {
+            build.onLoad(
+              { filter: /src\\.*\.js$/ },
+              async (args) => ({
+                loader: 'jsx',
+                contents: await fs.readFile(args.path, 'utf8'),
+              })
+            );
+          },
+        },
+      ],
+    },
+  },
 
 })
